cccf962107a72f761bf454f61dfca6353447d8c3
James Miller <james.fl.miller@live.co.uk>
2015-05-21 16:16:49 +0100

    Adds eyeTest matchers

--- makers-app/makers-appTests/makersAppFeatureTests.js
+++ makers-app/makers-appTests/makersAppFeatureTests.js
@@ -1,5 +1,8 @@
+#import "eyeTest.matchers.js"
+
 beginTest("When loaded the app says 'Hi!'");
     checkViewContainsText("Hi!");
+    checkViewContainsText("What's your name?'");
 endTest();
 
 beginTest("You can enter your name and tap the button")
