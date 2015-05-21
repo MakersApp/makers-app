94271e9ac109134c718afa2942e675e34cfefd9c
James Miller <james.fl.miller@live.co.uk>
2015-05-21 15:36:41 +0100

    Sets up initial file structure

---
+++ makers-app/makers-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  makers-app
+//
+//  Created by James Miller on 21/05/2015.
+//  Copyright (c) 2015 MakersApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
